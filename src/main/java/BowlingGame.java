85b20d01767dca7b352ca4e9e98bcf6a67c0e04d
Alec Reeves <alchreeves@gmail.com>
2017-06-01 15:53:17 -0700

    initial tests

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -2,7 +2,20 @@ package main.java;
 
 public class BowlingGame {
 
-	private int score = 0;
+	private int score;
+	
+	public BowlingGame(){
+		this.score = 0;
+	}
+
+	public void roll(int pins){
+		score += pins;
+	}
+	
+	public int getScore() {
+		
+		return score;
+	}
 	
 	
 	
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,21 +1,50 @@
 package test.java;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+import main.java.BowlingGame;
 
 
 public class BowlingGameTest {
-
+	
+	public BowlingGame bg = new BowlingGame();
+	
 	@Test
 	public void allMissesIsZero(){
 		//given
-		
+		bg.roll(0);
 		
 		//when
+		final int score = bg.getScore();
 		
 		//then
-		assertEquals(0, 0);
+		assertEquals(0, score);
+	}
+	
+	@Test
+	public void rollingOneGivesOne(){
+		//given
+		bg.roll(1);
+		
+		//when
+		final int score = bg.getScore();
+		
+		//then
+		assertEquals(1, score);
+	}
+	
+	@Test
+	public void rollingOneThenOneScoresTwo(){
+		//given
+		for(int i=0; i<2; i++){
+			bg.roll(1);
+		}
+		
+		//when
+		final int score = bg.getScore();
+		
+		//then
+		assertEquals(2, score);
 	}
 	
 }
